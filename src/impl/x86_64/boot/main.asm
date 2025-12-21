d211748558e985dad163573e2d2774bd8ed413d8
Joshua Washington <jwas249@wgu.edu>
2025-12-20 20:37:22 -0500

    added subroutines for long mode checking (64 bit)

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -3,6 +3,70 @@ global header_start
 section .text
 bits 32
 start:
-; print "OK"
+    mov esp, stack_top
+
+    call check_multiboot
+    call check_cpuid
+    call check_long_mode
+
+    ; print "OK"
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
+
+check_multiboot:
+    cmp eax, 0x36d76289
+    jne .no_multiboot
+    ret
+
+.no_multiboot:
+    mov al, "M"
+    jmp error
+
+check_cpuid:
+    pushfd
+    pop eax
+    mov ecx, eax
+    xor eax, 1 << 21
+    push eax
+    popfd
+    pushfd
+    pop eax
+    push ecx
+    popfd
+    cmp eax, ecx
+    je .no_cpuid
+    ret
+
+.no_cpuid:
+    mov al, "C"
+    jmp error
+
+check_long_mode:
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000001
+    jp .no_long_mode
+
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29
+    jz .no_long_mode
+
+    ret
+.no_long_mode:
+    mov al, "L"
+    jmp error
+
+error:
+    ; print "ERR: X" where X is the error code
+    mov dword [0xb800], 0x4f524f45
+    mov dword [0xb800], 0x4f3a4f52
+    mov dword [0xb800], 0x4f204f20
+    mov byte [0xb800a], al
+    hlt
+
+section .bss
+stack_bottom:
+    resb 4096 * 4
+
+stack_top:
