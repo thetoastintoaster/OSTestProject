c8c47ebf86028499a1d4be920fec7e715c594543
Joshua Washington <jwas249@wgu.edu>
2025-12-20 21:08:14 -0500

    added subroutines to set up paging for long mode

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -9,6 +9,9 @@ start:
     call check_cpuid
     call check_long_mode
 
+    call setup_page_tables
+    call_enable_paging
+
     ; print "OK"
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
@@ -57,6 +60,52 @@ check_long_mode:
     mov al, "L"
     jmp error
 
+setup_page_tables:
+    mov eax, page_table_13
+    or eax, 0b11 ; present, writable
+    mov [page_table_l4], eax
+
+    mov eax, page_table_12
+    or eax, 0b11 ; present, writable
+    mov [page_table_l3], eax
+
+    mov ecx, 0 ; counter
+.loop:
+
+    mov eax, 0x200000 ; 2MiB
+    mul ecx
+    or eax, 0b10000011 ; present, writable, huge page
+    mov [page_tabe_l2 + ecx * 8], eax
+
+    inc ecx ; increment counter
+    cmp ecx, 512 ; checks if the whole table is mapped
+    jne .loop ; if not, continue
+
+    ret
+
+enable_paging:
+    ; pass page table location to cpu
+    mov eax, page_table_l4
+    mov cr3, eax
+
+    ;enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ;enable long mode
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
+
 error:
     ; print "ERR: X" where X is the error code
     mov dword [0xb800], 0x4f524f45
@@ -66,6 +115,14 @@ error:
     hlt
 
 section .bss
+align 4096
+page_table_l4:
+    resb 4096
+page_table_l3:
+    resb 4096
+page_table_l2:
+    resb 4096 * 4
+
 stack_bottom:
     resb 4096 * 4
 
